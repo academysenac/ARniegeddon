c7380d89e11bc8729684166dc2fd496817bbcb68
WILLIAN HONDA <willianhonda@gmail.com>
2018-03-20 22:57:11 -0300

    Initial Commit

---
+++ ARniegeddon/Anchor.swift
@@ -0,0 +1,13 @@
+//
+//  Anchor.swift
+//  ARniegeddon
+//
+//  Created by Willian Honda on 20/03/18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import ARKit
+
+class Anchor: ARAnchor {
+  var type: NodeType?
+}
